eb96bf79224ec645a99e1c13a9b23ea551d070d8
jinajung810 <jjina810@gmail.com>
2023-10-01 04:14:00 +0900

    feat: filter by star rating

--- app/review-list.js
+++ app/review-list.js
@@ -3,21 +3,25 @@
 import { detailApi } from "@/pages/api/movies"
 import { useEffect, useState } from "react"
 import { Rating } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
 import MyReviewModal from "./MyReviewModal";
 import Link from "next/link";
 import {TbCubePlus } from 'react-icons/tb'
 
 export default function ReviewList({reviews}){
-  const parsedReviews = JSON.parse(reviews);
+  const initialReviews = JSON.parse(reviews);
   
   const [moviePosters, setMoviePosters] = useState([])
-  const [isModalOpen, setIsModalOpen] = useState(parsedReviews.map(() => false));
+  const [isModalOpen, setIsModalOpen] = useState(initialReviews.map(() => false));
   const [selectedReview, setSelectedReview] = useState(null);
+  const [selectedStar, setSelectedStar] = useState(null);
+  const [filteredReviews, setFilteredReviews] = useState(initialReviews); // 추가: 필터링된 리뷰 목록
 
   useEffect(()=>{
     const fetchMovie = async() =>{
       const posters = [];
-      for ( const review of parsedReviews ){
+      const filtered = initialReviews.filter(review => selectedStar === null || parseFloat(review.star) === selectedStar); // 별점 필터링 적용
+      for ( const review of filtered ){
         try {
           const movieDetails = await detailApi(review.movieId);
           if(movieDetails && movieDetails.poster_path) {
@@ -31,10 +35,11 @@ export default function ReviewList({reviews}){
           posters.push('Error')
         }
       }
-      setMoviePosters(posters)
+      setMoviePosters(posters);
+      setFilteredReviews(filtered); // 필터링된 리뷰 업데이트
     }
     fetchMovie();
-  },[])
+  }, [selectedStar]); // selectedStar 상태가 변경될 때마다 실행
 
   const openModal = (index) => {
     setSelectedReview(index);
@@ -46,7 +51,7 @@ export default function ReviewList({reviews}){
 
   const closeModal = (index) => {
     setSelectedReview(null);
-    const updatedIsModalOpen = Array(parsedReviews.length).fill(false);
+    const updatedIsModalOpen = Array(initialReviews.length).fill(false);
     setIsModalOpen(updatedIsModalOpen);
     document.body.style.overflow = "unset";
     window.location.reload(); // 페이지 새로고침
@@ -54,6 +59,24 @@ export default function ReviewList({reviews}){
   
   return (
     <div className="reviews">
+      <div className="star-list">
+        <span className="filter-label">Filter by :</span>
+        <Rating 
+        value={selectedStar}
+        onChange={(event, newValue) => {
+          // "전체 리뷰 보기"를 선택한 경우, selectedStar를 null로 설정
+          setSelectedStar(newValue === selectedStar ? null : newValue);
+        }}
+        precision={0.5}
+        emptyIcon={<StarIcon style={{ fill: 'transparent', stroke: '#faaf00' }}  fontSize="inherit" />} 
+        className="star-filter"/>
+        {/* "전체 리뷰 보기" 버튼 */}
+        <button
+          onClick={() => setSelectedStar(null)}
+          className="show-all-button">
+          Show All Reviews
+        </button>
+      </div>
       <Link href="/search">
         <div className="add-review">
           <TbCubePlus size='40'/>
@@ -63,7 +86,7 @@ export default function ReviewList({reviews}){
         </div>
       </Link>
       {
-        parsedReviews.map((review, index) => (
+        filteredReviews.map((review, index) => ( // 필터링된 리뷰 사용
           <div 
             onClick={() => openModal(index)}
             key={review._id} 
@@ -82,7 +105,7 @@ export default function ReviewList({reviews}){
         <MyReviewModal 
           isOpen={isModalOpen[selectedReview]}
           onClose={closeModal}
-          review={parsedReviews[selectedReview]}/>
+          review={initialReviews[selectedReview]}/>
       )}
     </div>
   )
