b1dbaf2f762d41bef48980aa48ac06a37445e1b0
jinajung810 <jjina810@gmail.com>
2023-09-30 19:14:11 +0900

    feat: color theme

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
+'use client'
+
 import SignInBtn from "./signInBtn";
 import SignUpBtn from "./signUpBtn";
 
+import { useEffect } from "react";
+
 export default function Home() {
+
+  useEffect(() => {
+    // 로컬 스토리지에서 테마 가져오기
+    const savedTheme = localStorage.getItem("selectedTheme");
+
+    // 가져온 테마가 있다면 적용
+    if (savedTheme) {
+      document.body.setAttribute("data-theme", savedTheme);
+    }
+  }, []); // 빈 배열을 넘겨주어 컴포넌트가 마운트될 때 한 번만 실행
 
   return (
     <div className="container login-page">
