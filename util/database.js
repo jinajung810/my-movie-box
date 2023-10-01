49eb851bf44962014ccc64771555df4e88eb1a34
jinajung810 <jjina810@gmail.com>
2023-10-01 17:55:19 +0900

    배포준비

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb'
 const url = 'mongodb+srv://admin:test1234@cluster0.lka1brg.mongodb.net/?retryWrites=true&w=majority'
 let connectDB
 
-if (process.env.local.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
     global._mongo = new MongoClient(url).connect()
   }
