854ba0c9ffc112248a482ddd0d8df1e3456fc5b0
jinajung810 <jjina810@gmail.com>
2023-10-01 19:56:37 +0900

    .

--- middleware.js
+++ middleware.js
@@ -6,14 +6,14 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request) {
 
   // 로그인 후 /main으로 이동 
-  if (request.nextUrl.pathname === '/') {
-    const session = await getToken({ req: request });
-    console.log('세션', session);
+  // if (request.nextUrl.pathname === '/') {
+  //   const session = await getToken({ req: request });
+  //   console.log('세션', session);
 
-    if (session) {
-      return NextResponse.redirect(new URL('/main', request.url));
-    }
-  }
+  //   if (session) {
+  //     return NextResponse.redirect(new URL('/main', request.url));
+  //   }
+  // }
   // /main에서 로그아웃 하면 다시 로그인 페이지('/')로 이동 
   if (request.nextUrl.pathname.startsWith('/main')) {
     const session = await getToken({ req : request })
