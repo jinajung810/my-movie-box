4daf4fa5626e51a7cde78184fd664c29b042c092
jinajung810 <jjina810@gmail.com>
2023-10-02 02:45:48 +0900

    fix: login page delete

--- app/page.js
+++ app/page.js
@@ -1,21 +1,91 @@
-'use client'
+import { connectDB } from "@/util/database";
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth/next";
+import Link from "next/link";
+import { SignOutBtn } from "../signOut";
+import SignInBtn from "../signInBtn";
+import ReviewList from "../review-list";
+import {TbCubePlus } from 'react-icons/tb'
 
-import SignInBtn from "./signInBtn";
+export default async function Home() {
+  let session = await getServerSession(authOptions);
+  let user = session ? session.user.email : null; 
 
+  if (!user) {
+    return (
+      <div className="container">
+        <div className="nav-bar">
+          <div className="logo">
+            Your <p>MOVIE</p> BOX
+          </div>
+          <div>
+            <SignInBtn />
+          </div>
+        </div>
+        <div className="my-list">
+          <div className="add-review">
+            <TbCubePlus size='40'/>
+            <h2 className="add-message">
+            It's an empty box.<br /> 
+            <span>After signing in,</span> <br />
+            search for a movie and add it.
+            </h2>
+          </div>
+        </div>
+        <div className='footer'>
+          <Link href='https://github.com/jinajung810/my-movie-box' target="_blank">
+            GitHub Repository
+          </Link>
+          &nbsp;・&nbsp;
+          <Link href='https://mail.google.com/mail/?view=cm&fs=1&to=${email}' target="_blank">Contact Me!</Link>
+          <span>{new Date().getFullYear()}</span>
+          <span>Jina Jung</span>
+        </div>
+      </div>
+    );
+  }
 
-export default function Home() {
+  const db = (await connectDB).db("my-movie-box");
+  let reviews = await db.collection('reviews').find().toArray();
+  const userReview = reviews.filter((review) => review.author === user);
+
+  const userReviews = userReview.map((review) => {
+    return {
+      _id: review._id.toString(),
+      author: review.author,
+      movieId: review.movieId,
+      date: review.date,
+      where: review.where,
+      with: review.with,
+      star: review.star,
+      favoriteLine: review.favoriteLine,
+      memo: review.memo,
+    };
+  });
 
   return (
-    <div className="container login-page">
-      <div className="headline">
-        <h1>
-          Record<br />
-          the <span>MOVIE</span>       
-          I watched
-        </h1>
+    <div className="container">
+      <div className="nav-bar">
+        <div className="logo">
+          {session.user ? `${session.user.name}'s ` : 'Your '}
+          <span>MOVIE</span> BOX
+        </div>
+        <div>
+          <SignOutBtn />
+        </div>
       </div>
-      <SignInBtn />
-
+      <div className="my-list">
+        <ReviewList reviews={JSON.stringify(userReviews)} />
+      </div>
+      <div className='footer'>
+        <Link href='https://github.com/jinajung810/my-movie-box' target="_blank">
+          GitHub Repository
+        </Link>
+        &nbsp;・&nbsp;
+        <Link href='https://mail.google.com/mail/?view=cm&fs=1&to=${email}' target="_blank">Contact Me!</Link>
+        <span>{new Date().getFullYear()}</span>
+        <span>Jina Jung</span>
+      </div>
     </div>
-  )
+  );
 }
